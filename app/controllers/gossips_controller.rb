101a0f25667810c491b372d726dfea350f1e428d
stephan-dev <stephan-dev@outlook.fr>
2018-11-01 13:38:24 +0100

    index, edit, delete

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -13,4 +13,27 @@ class GossipsController < ApplicationController
 	def show
 		@gossip = Gossip.find(params[:id])
 	end
+
+	def edit
+		@gossip = Gossip.find(params[:id])
+	end
+
+	def update
+	  @gossip = Gossip.find(params[:id])
+
+		gossip_params = params.require(:gossip).permit(:anonymous_gossiper, :content)
+		@gossip.update(gossip_params)
+
+		redirect_to gossips_path
+	end
+
+	def index
+		@gossip = Gossip.all
+	end
+
+	def destroy
+		@gossip = Gossip.find(params[:id])
+		@gossip.destroy
+		redirect_to gossips_path
+	end
 end
